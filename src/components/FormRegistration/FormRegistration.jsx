6037767ef5d66daada695f373a2ab76dbd7e310b
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-03-03 14:19:52 +0200

    structure components with module css

---
+++ src/components/FormRegistration/FormRegistration.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FormRegistration = () => {
+  return <div>FormRegistration</div>;
+};
---
+++ src/components/RestorationModal/RestorationModal.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const RestorationModal = () => {
+  return <div>RestorationModal</div>;
+};
